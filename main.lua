9de2453e21879bf543b9bf5ccc10d2e7145c1237
= <=>
2024-08-12 13:55:53 +0800

    i cant test rn, but lemme commit in case

--- main.lua
+++ main.lua
@@ -1,14 +1,31 @@
 function love.load()
     require("player")
     require("floor")
+    require("pillar")
     Object = require "classic"
+    pillars = {
+        Pillar(1440 + 180 * 0),
+        Pillar(1440 + 180 * 1),
+        Pillar(1440 + 180 * 2),
+        Pillar(1440 + 180 * 3),
+        Pillar(1440 + 180 * 4),
+        Pillar(1440 + 180 * 5),
+        Pillar(1440 + 180 * 6),
+        Pillar(1440 + 180 * 7),
+    }
 end
 
 function love.update(dt)
     player.update(player, dt)
+    for i = 1, #pillars do
+        pillars[i].update()
+    end
 end
 
 function love.draw()
     floor.draw(floor)
     player.draw(player)
+    for i = 1, #pillars do
+        pillars[i].draw()
+    end
 end
--- pillar.lua
+++ pillar.lua
@@ -1,8 +1,8 @@
 Pillar = Object.extend(Object)
 
-function Pillar.new(self, xPos, yPos)
+function Pillar.new(self, xPos)
     self.xPos = xPos or 1440
-    self.yPos = yPos
+    self.yPos = math.random(0, 800)
     self.width = 75
     self.height = 350
     self.velocity = 150
@@ -10,4 +10,13 @@ end
 
 function Pillar.update(self, dt)
     self.xPos = self.xPos + self.velocity * dt
+    if self.xPos < 0 then
+        self.xPos = 1440
+        self.yPos = math.random(0, 800)
+    end
+end
+
+function Pillar.draw(self)
+    love.graphics.rectangle("fill", self.xPos, 0, self.width, self.yPos)
+    love.graphics.rectangle("fill", self.xPos, self.yPos + 100, self.width, 900)
 end
